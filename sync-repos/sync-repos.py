93cdf1cd9613f3566da1b6d1498621f4b465dd89
maneamarius <maneamarius@gmail.com>
2019-03-16 13:49:53 -0400

    print exception message

--- sync-repos/sync-repos.py
+++ sync-repos/sync-repos.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+# requires GitPython pip package to be installed
 
 import os
 import yaml
@@ -17,11 +19,12 @@ for k, v in repos_data.items():
             try:
                 g = git.cmd.Git(folder)
                 g.pull()
-            except:
-                print("   - failed pulling!")
+            except Exception as e:
+                print("   - failed pulling! The error message was: " + str(e))
         else:
             repo = "https://git-url/scm/" + folder + ".git"
+            print(repo)
             try:
                 git.Git(k).clone(repo)
-            except:
-                print("   - failed cloning!")
+            except Exception as e:
+                print("   - failed cloning! The error message was: " + str(e))
