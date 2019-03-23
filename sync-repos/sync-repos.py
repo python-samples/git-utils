500076b756ff62248a3111c8812450895d2564e9
Marius Manea <maneamarius@gmail.com>
2019-03-23 16:40:31 -0400

    parameterize server_url and move it in the repos.yml file; various improvements

--- sync-repos/sync-repos.py
+++ sync-repos/sync-repos.py
@@ -6,25 +6,34 @@ import os
 import yaml
 import git
 
-with open("repos.yml", 'r') as stream:
+script_dir = os.path.dirname(__file__)
+#work_dir=os.getcwd()
+
+with open(os.path.join(script_dir,"repos.yml"), 'r') as stream:
     repos_data = yaml.load(stream)
 
-for k, v in repos_data.items():
-    if not os.path.isdir(k):
-        os.mkdir(k)
-    for repo in v:
-        folder = k + "/" + repo
-        print(folder)
-        if os.path.isdir(folder):
+server_url = repos_data['server_url']
+
+for project, repos in repos_data['repos'].items():
+    project_path = os.path.join(script_dir,project)
+    if not os.path.isdir(project_path):
+        os.mkdir(project_path)
+    for repo in repos:
+        repo_path = os.path.join(project_path,repo)
+        print(repo_path)
+        if os.path.isdir(repo_path):
             try:
-                g = git.cmd.Git(folder)
-                g.pull()
+                os.system('cd "' + repo_path + '" && git branch -r | grep -v \'\->\' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done')
+                os.system('cd "' + repo_path + '" && git fetch --all')
+                os.system('cd "' + repo_path + '" && git pull --all')
+#                g = git.cmd.Git(folder)
+#                g.pull()
             except Exception as e:
                 print("   - failed pulling! The error message was: " + str(e))
         else:
-            repo = "https://git-url/scm/" + folder + ".git"
-            print(repo)
+            repository = server_url + project + "/" + repo + ".git"
+            print(repository)
             try:
-                git.Git(k).clone(repo)
+                git.Git(project_path).clone(repository)
             except Exception as e:
                 print("   - failed cloning! The error message was: " + str(e))
